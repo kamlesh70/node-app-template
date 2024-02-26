434e3ab9ed0171295dadfe96c74a76770c3b8f4b
kamlesh70 <kamalmehra3571@gmail.com>
2024-02-26 09:01:00 +0530

    add .env.example

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -2,11 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
 import logger from "../config/logger";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const globalErrorHandler = (
   err: HttpError,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction,
 ) => {
   logger.error(err?.message);
