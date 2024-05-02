753b93d727b31023519b6f9c1059dd5ac6552260
kamlesh70 <kamalmehra3571@gmail.com>
2024-05-02 06:37:24 +0530

    update global error handler

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,23 +1,37 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { HttpError } from "http-errors";
+import { v4 as uuidv4 } from "uuid";
 import logger from "../config/logger";
 
 const globalErrorHandler = (
   err: HttpError,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction,
 ) => {
-  logger.error(err?.message);
-  const statusCode = err.statusCode || 500;
+  const statusCode = err.status || 500;
+  const isProduction = process.env.NODE_ENV === "production";
+  const message = isProduction ? "Internal server error" : err.message;
+  logger.error({
+    errorId: uuidv4(),
+    type: err.name,
+    msg: message,
+    path: req.path,
+    method: req.method,
+    stack: err.stack,
+  });
   res.status(statusCode).json({
     errors: [
       {
+        errorId: uuidv4(),
         type: err.name,
-        message: err.message,
-        path: "",
-        location: "",
+        msg: message,
+        path: req.path,
+        method: req.method,
+        stack: isProduction ? null : err.stack,
       },
     ],
   });
